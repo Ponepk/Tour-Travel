6d0734aa5191c36a257d6298455a48e7c21a0dc4
Pawan Vishwakarma <135411482+Ponepk@users.noreply.github.com>
2024-03-14 12:04:22 +0530

    Add files via upload

---
+++ travel/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import './Component/Navbar/Navbar.jsx'
+import Navbar from './Component/Navbar/Navbar'
+import Home from './Component/Home/Home.jsx'
+import Main from './Component/Main/Main.jsx'
+import Footer from './Component/Footer/Footer.jsx'
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Home/>
+   <Main/>
+   <Footer/>
+   </>
+  );
+}
+
+export default App;
